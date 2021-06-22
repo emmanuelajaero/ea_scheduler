34b9d263adebe5dca79004640126e04d4aa19629
emmanuel <emmyprinso@gmail.com>
2021-06-22 06:14:14 +0100

    connected the trigger action to the navbar

--- src/components/nav-bar/navbar.js
+++ src/components/nav-bar/navbar.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { toggleAddWorkOrder } from '../../store/actions/triggers'
+import { useDispatch, useSelector } from 'react-redux'
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
-import MailIcon from '@material-ui/icons/Mail';
 import ListIcon from '@material-ui/icons/List';
 import LayersClearIcon from '@material-ui/icons/LayersClear';
 import LayersIcon from '@material-ui/icons/Layers';
@@ -9,10 +10,24 @@ import LayersIcon from '@material-ui/icons/Layers';
 import classes from './navbar.module.css'
 
 function EA_NavBar() {
+    const dispatch = useDispatch()
+    
+  
+    // useEffect(() => {
+    // }, [])
+
+    // console.log("addWorkOrder :> ", addWorkOrder)
     return (
         <nav>
             <div style={{display: "flex"}}>
-                <IconButton aria-label="delete" className={classes.listButton}>
+                <IconButton 
+                    aria-label="Show Side Bar" 
+                    className={classes.listButton}
+                    onClick={() => {
+                        dispatch(toggleAddWorkOrder());
+                        console.log("List Clicked")
+                    }}
+                >
                     <ListIcon  />
                 </IconButton>
 
