908dc11b9eca5072309bce6ea63cd240f034bfca
emmanuel <emmyprinso@gmail.com>
2021-06-22 06:18:29 +0100

    add worker to the system

---
+++ src/components/add-workorder/add-workorder.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from './add-workorder.module.css'
+
+function AddWorkOrder() {
+    return (
+        <div className={classes.addWorkOrderContainer}>
+            
+        </div>
+    )
+}
+
+export default AddWorkOrder
